1247c0537c5709adaf4e76df5e7cbbc021d2c80f
Didier Verna <didier@didierverna.net>
2015-09-28 11:54:36 +0200

    Add the demos/ directory.
    2015-09-28  Didier Verna  <didier@didierverna.net>
    
    	* demos/quickstart.lisp: New file.

---
+++ demos/quickstart.lisp
@@ -0,0 +1,60 @@
+;;; quickstart.lisp --- Basic usage demonstration program
+
+;; Copyright (C) 2015 Didier Verna
+
+;; Author: Didier Verna <didier@didierverna.net>
+
+;; This file is part of FoCus.
+
+;; Permission to use, copy, modify, and distribute this software for any
+;; purpose with or without fee is hereby granted, provided that the above
+;; copyright notice and this permission notice appear in all copies.
+
+;; THIS SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+(in-package :cl-user)
+
+(require "asdf")
+(asdf:load-system :net.didierverna.focus)
+(net.didierverna.focus:nickname-package)
+
+
+(defpackage :quotation
+  (:use :cl)
+  (:export :quotation))
+
+(in-package :quotation)
+
+(defun quotation-formatter (stream argument colonp atsignp &rest arguments)
+  (declare (ignore colonp atsignp arguments))
+  (write-char #\` stream)
+  (write-string argument stream)
+  (write-char #\' stream))
+
+(focus:make-format-table :name :quotation)
+(focus:set-format-directive #\`
+  :function 'quotation-formatter :table :quotation)
+
+(defun quotation (who quotation)
+  (focus:with-format-table :quotation
+    (focus:format t "As ~A would say: ~`.~%" who quotation)))
+
+
+(in-package :cl-user)
+
+(quotation:quotation "Bugs Bunny" "Errr, what's up Doc?")
+
+;;; quickstart.lisp ends here
